9010345d15171d7ed5d7ac751a4adc04fe283467
Aidan Randle-Conde <aidan.randleconde@gmail.com>
2021-05-02 18:30:58 +0100

    Added code to show lab production bonus and research per tick.

--- src/mods/showefficiency.js
+++ src/mods/showefficiency.js
@@ -8,6 +8,43 @@ const REVERSE_EFFICIENCIES = [
   'sorterHorizontal',
   'garbageCollector',
 ];
+const PRODUCTION_BONUS_BUILDINGS = [
+  'researchCenter',
+  'researchCenter2',
+  'researchCenter3',
+  'researchCenter4',
+  'metalsLab',
+  'gasAndOilLab',
+  'qualityLab'
+];
+const BUILDING_COLORS = {
+  'researchCenter': 'rgb(54,140,198)',
+  'researchCenter2': 'rgb(0,159,131)',
+  'researchCenter3': 'rgb(205,106,70)',
+  'researchCenter4': 'rgb(122,209,222)',
+  'metalsLab': 'rgb(76,97,168)',
+  'gasAndOilLab': 'rgb(0,159,131)',
+  'analystCenter': 'rgb(205,106,70)',
+  'qualityLab': 'rgb(242,127,143)'
+};
+const RESEARCH_CENTER_BUILDINGS = [
+  'researchCenter',
+  'researchCenter2',
+  'researchCenter3',
+  'researchCenter4',
+];
+const RESEARCH_MULTIPLIERS = {
+  'researchCenter': 4 / 10.0,
+  'researchCenter2': 6 / 20.0,
+  'researchCenter3': 32 / 40.0,
+  'researchCenter4': 1200 / 40.0
+};
+const RESEARCH_BASES = {
+  'researchCenter:' 1 / 10.0,
+  'researchCenter2': 4 / 20.0,
+  'researchCenter3': 20 / 40.0,
+  'researchCenter4': 100 / 40.0
+};
 
 Fimod.define({
   name: "showefficiency",
@@ -102,12 +139,46 @@ Fimod.define({
       const meta = component.getMeta();
 
       const reverse = REVERSE_EFFICIENCIES.indexOf(meta.id) !== -1;
+      const show_production_bonus = PRODUCTION_BONUS_BUILDINGS.indexOf(meta.id) !== -1;
+      const is_research_center = RESEARCH_CENTER_BUILDINGS.indexOf(meta.id) !== -1;
       if (reverse) efficiency = 100 - efficiency;
 
       const size = this.tileSize;
       const iconSize = (size / 6);
       const x = tile.getX() * size + (iconSize * 1.5);
       const y = (tile.getY() + meta.height) * size - (iconSize * 1.5);
+
+      context.save();
+      if (show_production_bonus) {
+        var dh = 1.0 * size;
+        if (is_research_center) {
+          dh += 0.5 * size;
+        }
+        var rx = tile.getX() * size + padding;
+        var rw = meta.width * size - 2 * padding;
+        var rh = dh - 2 * padding;
+        var ry = (tile.getY() + meta.height) * size + padding - dh;
+
+        var bonus = component.strategy.productionBonus;
+        var text_bonus = bonus + "  PB";
+
+        context.fillStyle = "white";
+        context.strokeStyle = BUILDING_COLORS[meta.id];
+        context.lineWidth = padding;
+        context.fillRect(rx, ry, rw, rh);
+        context.strokeRect(rx, ry, rw, rh);
+
+        context.textAlign = "right";
+        context.fillStyle = BUILDING_COLORS[meta.id];
+        context.fillText(text_bonus, (tile.getX() + meta.width) * size - 2 * padding, y);
+
+        if (is_research_center) {
+          var research = RESEARCH_BASES[meta.id] + (bonus * RESEARCH_MULTIPLIERS[meta.id]);
+          var text_research = research.toFixed(1) + " R per T";
+          context.fillText(text_research, (tile.getX() + meta.width) * size - 2 * padding, y - 0.5 * size);
+        }
+      }
+      context.restore();
 
       context.fillStyle = colors[((colors.length - 1) * (efficiency / 100)).toFixed()];
       context.beginPath();
